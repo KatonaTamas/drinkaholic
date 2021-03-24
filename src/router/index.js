2f17602b6af4707a2c568f13fbe3e176ac0285e5
KatonaTamas <55735254+KatonaTamas@users.noreply.github.com>
2021-03-25 00:10:07 +0100

    Created Main Routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import AllBeers from '../views/AllBeers.vue';
+import SearchArea from '../views/SearchArea.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'SearchArea',
+    component: SearchArea
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/all',
+    name: 'AllBeers',
+    component: AllBeers
   }
 ]
 
